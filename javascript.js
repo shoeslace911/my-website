6c47373212ce674b21a4e635d1038f574a89fb8a
Sho Fujiwara <shofujiwara505@gmail.com>
2022-02-16 17:34:34 +0900

    Added cool parallax animations

--- javascript.js
+++ javascript.js
@@ -8,13 +8,40 @@ let overlayOpen = false;
 button.addEventListener("click", openOverlay);
 function openOverlay(){
     if(overlayOpen === false){
+
         overlayNav.classList.add("open");
         hamburger.classList.add("X");
         overlayOpen = true;
+
     }else{
+
         overlayNav.classList.remove("open");
         hamburger.classList.remove("X");
         overlayOpen = false;
+        
     }
 
 }
+
+window.addEventListener("scroll", function(e){
+
+    const target = document.querySelector(".sample-img2");
+
+    var scrolled = window.pageYOffset;
+    var rate = scrolled * -0.1;
+
+    target.style.transform = "translate3d(2vmax,"+rate+"px, 0)";
+
+});
+
+window.addEventListener("scroll", function(e){
+
+    const target = document.querySelector(".diamond-casino-img");
+
+    
+    var scrolled = window.pageYOffset;
+    var rate = scrolled * 0.1;
+
+    target.style.transform = "translate3d(0vmax,"+rate+"px, 0)";
+});
+
